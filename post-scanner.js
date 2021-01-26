f224006a5beaae942fa12b50a6d626437fba442a
koukol <webjunkregs@gmail.com>
2021-01-26 17:56:09 +0100

    pause YT videos

--- post-scanner.js
+++ post-scanner.js
@@ -66,6 +66,7 @@ function scanPosts() {
         // to make youtube embeds work
         jQuery("div.youtube").on("click", function() {
             var iframe = document.createElement("iframe");
+            iframe.setAttribute("class", "youtube-iframe");
             iframe.setAttribute("frameborder", "0");
             iframe.setAttribute("allowfullscreen", "");
             iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
