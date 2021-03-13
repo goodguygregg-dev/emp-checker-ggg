17066647e59d0e3a1c1fae3b339d10c7e46f5136
goodguygregg-dev <75133660+goodguygregg-dev@users.noreply.github.com>
2021-03-13 21:44:57 +0100

    Update Main_UserScript.user.js
    update urls

--- Main_UserScript.user.js
+++ Main_UserScript.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         FLS Comment Checker MAIN
 // @namespace    http://tampermonkey.net/
-// @version      1.93
+// @version      1.94
 // @description  Credits to curtwagner who wrote and maintained the original script.
 // @author       goodguygregg
-// @match        /^https://www\.empornium\.(me|sx|is)/torrents.php\?*action=allcomments*
-// @include      /^https://www\.empornium\.(me|sx|is)/torrents.php\?*action=allcomments*
-// @include      /^https://www\.empornium\.(me|sx|is)/requests.php\?*action=allcomments*
-// @include      /^https://www\.empornium\.(me|sx|is)/forum/recent*/
-// @include      /^https://www\.empornium\.(me|sx|is)/collages.php\?*action=allcomments*
+// @match        https://www.empornium.*/torrents.php*allcomments
+// @include      https://www.empornium.*/torrents.php*allcomments
+// @include      https://www.empornium.*/requests.php*allcomments
+// @include      https://www.empornium.*/forum/recent*
+// @include      https://www.empornium.*/collages.php*allcomments
 // @downloadURL  https://github.com/goodguygregg-dev/emp-checker-ggg/raw/main/Main_UserScript.user.js //
 // @grant        GM_setValue
 // @grant        GM_getValue
